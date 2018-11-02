f9aab284b50ffaf1be6fcaf64d6d0f72d138c886
Simon Lipke <simon.lipke@googlemail.com>
2018-11-02 14:24:33 +0100

    [TASK] Build v1beta1

--- generation/generate_client_libs.sh
+++ generation/generate_client_libs.sh
@@ -1,5 +1,5 @@
 # Download v1alpha1 grafeas.swagger.json.
-wget https://github.com/grafeas/grafeas/raw/master/v1alpha1/proto/grafeas.swagger.json
+wget https://raw.githubusercontent.com/grafeas/grafeas/master/proto/v1beta1/swagger/grafeas.swagger.json
 
 # Download swagger-codegen CLI tool.
 wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar -O swagger-codegen-cli.jar
@@ -8,5 +8,5 @@ wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagge
 java -jar ./swagger-codegen-cli.jar generate \
     -i ./grafeas.swagger.json \
     -l go \
-    -o ../v1alpha1 \
+    -o ../v1beta1 \
     -c ./config.go.json \
